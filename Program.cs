40f7386d87c4230b807bda4570a51dd1a87e54b3
eugenio-chimuco <eugenio-2011@live.com.pt>
2024-09-24 14:32:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ Smartphone nokia= new Nokia (numero:"1234543",modelo:"modelo N250",imei:"0986514
 nokia.Ligar();
 nokia.InstalarAplicativo("Watsapp");
 Console.WriteLine("\n");
-Console.WriteLine("Smartphone Nokia:");
+Console.WriteLine("Smartphone Iphone:");
 Smartphone iphone = new Iphone(numero: "976541", modelo: "modelo I03", imei: "0001110002", memoria: 32);
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Telegram");
