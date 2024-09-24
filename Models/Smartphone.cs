9067aeb7a7831c3a105ef13bc4c520003fc2eee3
eugenio-chimuco <eugenio-2011@live.com.pt>
2024-09-24 14:22:26 +0100

    Adicionar Poo Resolvido

---
+++ Models/Smartphone.cs
@@ -0,0 +1,35 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        
+      
+        public Smartphone(string numero,string modelo,string imei,int memoria)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+        public string Numero { get; set; }
+
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama;
+  
+       private string Modelo;
+       private string Imei;
+       private int Memoria;
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia= new Nokia (numero:"1234543",modelo:"modelo N250",imei:"09865145",memoria:19);
+nokia.Ligar();
+nokia.InstalarAplicativo("Watsapp");
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "976541", modelo: "modelo I03", imei: "0001110002", memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
